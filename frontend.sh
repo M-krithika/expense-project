dc07ed372945bc3269078d2a0aa99172d06181cf
M-krithika <krithikanani8@gmail.com>
2024-04-24 17:43:58 +0530

    backend shell script

---
+++ frontend.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[3m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[35m"
+
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $n"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [$USERID -ne 0 ]
+then
+    echo "please run the script with root access"
+else
+    echo "you are super user"
+fi
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "iEnabiling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "start nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "removing existing content from nginx website"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Exacting frontend code"
+
+cp /home/ec2-user/expense-project/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "copied expense conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "restarting nginx"
