6b681c207b2ac9426f4bdc2edd41ee5927afade9
M-krithika <krithikanani8@gmail.com>
2024-04-24 15:49:28 +0530

    backend shell script

---
+++ backend.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[33m"
+Y="\e[33m"
+B="\e[34m"
+P="\e[35m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+
+    else
+        echo -e "$2...$G SUCCESS $N"
+
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access."
+    exit 1
+else
+    echo "you are super user"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "disabiling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "enabiling nodejs"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "instailing nodejs"
+
+useradd expense
+VALIDATE $? "adding user expense"
